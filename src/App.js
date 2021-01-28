457d5f1815866f4c1f303fdcac90c560948547b3
Knight <mayanks365@gmail.com>
2021-01-28 14:28:52 +0530

    updating class files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import SeasonDisplay from './SeasonDisplay'
+import React from 'react'
 
-function App() {
-  // shows user location
-  // two argument
-  // run 1 callback function on sucess
-  // run 2 callback function on on failure
-  window.navigator.geolocation.getCurrentPosition(
+class App extends React.Component{
+  render(){
+    window.navigator.geolocation.getCurrentPosition(
     ( position ) => { console.log( position )},
     ( err ) => console.log(err)
   )
@@ -14,6 +12,23 @@ function App() {
       <SeasonDisplay/>
     </div>
   );
+  }
 }
+
+// function App() {
+//   // shows user location
+//   // two argument
+//   // run 1 callback function on sucess
+//   // run 2 callback function on on failure
+//   window.navigator.geolocation.getCurrentPosition(
+//     ( position ) => { console.log( position )},
+//     ( err ) => console.log(err)
+//   )
+//   return (
+//     <div className="App">
+//       <SeasonDisplay/>
+//     </div>
+//   );
+// }
 
 export default App;

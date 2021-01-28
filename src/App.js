dc2c92c3499da7334437a3c6c21ed49812d871c4
Knight <mayanks365@gmail.com>
2021-01-28 14:20:05 +0530

    updating

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import SeasonDisplay from './SeasonDisplay'
 
 function App() {
+  // shows user location
+  // two argument
+  // run 1 callback function on sucess
+  // run 2 callback function on on failure
+  window.navigator.geolocation.getCurrentPosition(
+    ( position ) => { console.log( position )},
+    ( err ) => console.log(err)
+  )
   return (
     <div className="App">
       <SeasonDisplay/>
